2f95be0011f5c4af577305f8f9767c279159ce56
LuizJarduli <luizjarduli@gmail.com>
2020-10-24 15:23:34 -0300

    AlphaProt1.2

---
+++ Prototipo_EngSoft/frmHistorico.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+namespace Protótipo_EngSoft
+{
+    public partial class frmHistorico : Form
+    {
+        public frmHistorico()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHistorico_Load(object sender, EventArgs e)
+        {
+            DirectoryInfo directory = new FileInfo("../../planilhas/").Directory;
+            //MessageBox.Show("" + directory);
+            String name = "Planilha1";
+            String constr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
+                            directory +
+                            "/Tabela_vendas.xlsx;Extended Properties='Excel 12.0 XML;HDR=YES;';";
+
+            OleDbConnection con = new OleDbConnection(constr);
+            OleDbCommand oconn = new OleDbCommand("Select * From [" + name + "$]", con);
+            con.Open();
+
+            OleDbDataAdapter sda = new OleDbDataAdapter(oconn);
+            DataTable data = new DataTable();
+            sda.Fill(data);
+            dgvExcel.DataSource = data;
+        }
+    }
+}
---
+++ Prototipo_EngSoft/frmPrincipal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Protótipo_EngSoft
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void entregasRealizadasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pbFundo.Hide();
+            Form fc = Application.OpenForms["frmHistorico"];
+            if (fc != null)
+            {
+                fc.Close();
+            }
+            frmHistorico FrmHistorico = new frmHistorico();
+            FrmHistorico.MdiParent = this; // anexar o form 'filho' dentro do form 'pai'
+            FrmHistorico.WindowState = FormWindowState.Maximized;
+            FrmHistorico.Show();
+        }
+    }
+}
